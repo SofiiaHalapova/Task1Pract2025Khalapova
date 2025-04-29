ccdfe4b613564eb471210c95d31d916082751ce8
Sofiia_Halapova <Sonya05112014@gmail.com>
2025-04-29 14:14:42 +0300

    move llm logic into views

--- website/views.py
+++ website/views.py
@@ -1,6 +1,10 @@
 from flask import Blueprint, render_template, redirect
+import requests
 
 views = Blueprint('views', __name__)
+
+LM_STUDIO_URL = "http://localhost:1234/v1/chat/completions"
+LLM_NAME = "mistral-7b-instruct-v0.3"
 
 
 @views.route('/', methods=['GET'])
@@ -11,3 +15,25 @@ def view():
 @views.route('/gradio')
 def gradio():
     return redirect("http://localhost:7860")
+
+
+def chat(user_input):
+    if not user_input:
+        return "error: No user_input"
+
+    data = {
+        "model": LLM_NAME,
+        "messages": [{"role": "user", "content": user_input}]
+    }
+
+    try:
+        response = requests.post(LM_STUDIO_URL, json=data)
+
+        response.raise_for_status()
+
+        response_data = response.json()
+        answer = response_data['choices'][0]['message']['content']
+
+        return answer
+    except Exception as e:
+        return f"Exception: {str(e)}"
